ae2a4fa14a067544e6a4bf098a939c0d0ac49d8d
Zachary Voase <zvoase@twitter.com>
2013-10-14 08:39:17 -0700

    Remove distribute_setup.py; code style improvements.

--- setup.py
+++ setup.py
@@ -1,30 +1,12 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import os
-import re
-
-from distribute_setup import use_setuptools; use_setuptools()
-from setuptools import setup, find_packages
-
-
-rel_file = lambda *args: os.path.join(os.path.dirname(os.path.abspath(__file__)), *args)
-
-def read_from(filename):
-    fp = open(filename)
-    try:
-        return fp.read()
-    finally:
-        fp.close()
-
-def get_version():
-    data = read_from(rel_file('src', 'cssmin.py'))
-    return re.search(r"__version__ = '([^']+)'", data).group(1)
+from setuptools import setup
 
 
 setup(
     name             = 'cssmin',
-    version          = get_version(),
+    version          = '0.1.4',
     author           = "Zachary Voase",
     author_email     = "zacharyvoase@me.com",
     url              = 'http://github.com/zacharyvoase/cssmin',
